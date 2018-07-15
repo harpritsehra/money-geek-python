69f6ac5ca8138e74bf5ae9e69ad7b20963972bed
harpritsehra <harpritsehra@hotmail.com>
2018-07-15 11:37:54 -0400

    supported types

---
+++ billProcessor/static_data/types.sql
@@ -0,0 +1,3 @@
+insert into type(enum, description) values("HSBC_CC", "HSBC Credit Card");
+insert into type(enum, description) values("Virgin_CC", "Virgin BOA Credit Card");
+insert into type(enum, description) values("HSBC_CHK", "HSBC Checking");
