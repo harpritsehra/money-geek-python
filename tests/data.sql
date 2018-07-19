22576831b8a8492003bc7cd5b649943efccccdbf
harpritsehra <harpritsehra@hotmail.com>
2018-07-19 00:05:39 -0400

    tests

---
+++ tests/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user(username, password, first, last)
+VALUES ("testuser", "pbkdf2:sha256:50000$UsNfOdqf$e16f7d5eb083c29cc058b4faac66ee893bfd3a2463dd0339a3e21255c5e249b1", "test", "user");
