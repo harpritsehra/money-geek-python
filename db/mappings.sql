cda9331e5443c50c97c9252dad54654b14a2a8db
harpritsehra <harpritsehra@hotmail.com>
2018-12-09 23:32:39 -0500

    Removing

--- db/mappings.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO mapping(pattern, categoryID) VALUES("PAYMENT - THANK YOU", 30);
-INSERT INTO mapping(pattern, categoryID) VALUES("PAYPAL *MSA%", 22);
-INSERT INTO mapping(pattern, categoryID) VALUES("HLU*HULU%", 12);
-INSERT INTO mapping(pattern, categoryID) VALUES("SALARY DEPOSIT%", 32);
-INSERT INTO mapping(pattern, categoryID) VALUES("%TRANSFER TO SAVINGS%", 33);
-INSERT INTO mapping(pattern, categoryID) VALUES("%PAYMENT TO CON ED%", 26);
-INSERT INTO mapping(pattern, categoryID) VALUES("USA*USA*SNACK SODA%", 4);
-INSERT INTO mapping(pattern, categoryID) VALUES("ARAMARK GOLDMAN SACHS%", 4);
-INSERT INTO mapping(pattern, categoryID) VALUES("LYFT *RIDE%", 16);
